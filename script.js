078fe49d7c1cc7001b01e3575f0407c00b3ca1ac
Gopal <gopalkaran2021@gmail.com>
2021-08-16 23:08:20 +0530

    added mobile version

---
+++ script.js
@@ -0,0 +1,8 @@
+// const checkBox = document.querySelector('#nav-checkbox');
+// checkBox.addEventListener('click', () => {
+//     const navElement = checkBox.parentElement;
+//     // const Header = navElement.parentElement;
+//     navElement.classList.toggle('dark');
+//     const boxHeading = Header.children;
+//     boxHeading[1].classList.toggle('hide');
+// })
