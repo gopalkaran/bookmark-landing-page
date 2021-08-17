4a3efd5ec2f4fa84ea5c37c9a2f9c8a811cd4b0e
Gopal <gopalkaran2021@gmail.com>
2021-08-17 22:53:22 +0530

    modified mobile version of dropdown navbar

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-// const checkBox = document.querySelector('#nav-checkbox');
-// checkBox.addEventListener('click', () => {
-//     const navElement = checkBox.parentElement;
-//     // const Header = navElement.parentElement;
-//     navElement.classList.toggle('dark');
-//     const boxHeading = Header.children;
-//     boxHeading[1].classList.toggle('hide');
-// })
+const checkBox = document.querySelector('#nav-checkbox');
+checkBox.addEventListener('click', () => {
+    const navElement = checkBox.parentElement;
+    navElement.classList.toggle('modal-container');
+    
+    const navChildren = navElement.children;
+    const titleDiv = navChildren[0];
+    const titleSVG = titleDiv.children;
+    const titleG = titleSVG[0].children;
+    const titlePath = titleG[0].children;
+    titlePath[0].classList.toggle('modal');
+
+
+    const menuAndClose = navChildren[2];
+    const SVG = menuAndClose.children;
+    // SVG[0].classList.toggle('modal');
+    SVG[1].classList.toggle('modal');
+
+})
