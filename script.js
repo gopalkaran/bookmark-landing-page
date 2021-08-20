03c4d72c0b5fa07ed9d006a66b473ac628cd9b57
Gopal <gopalkaran2021@gmail.com>
2021-08-20 10:48:41 +0530

    created tab view

--- script.js
+++ script.js
@@ -17,3 +17,33 @@ checkBox.addEventListener('click', () => {
     SVG[1].classList.toggle('modal');
 
 })
+
+
+const tabs = document.querySelectorAll('[data-tab-target]')
+console.log(tabs)
+const tabContentImgList = document.querySelectorAll('[data-tab-content-img]')
+const tabContentInfoList = document.querySelectorAll('[data-tab-content-info]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', ()=>{
+        const tabContentId = tab.dataset.tabTarget.split(" ");
+        console.log(tabContentId)
+        const tabContentImg = document.querySelector(tabContentId[0]);
+        const tabContentInfo = document.querySelector(tabContentId[1]);
+        tabContentImgList.forEach(img => {
+            img.classList.remove('active');
+        })
+        tabContentInfoList.forEach(info => {
+            info.classList.remove('active');
+        })
+        tabContentImg.classList.add('active');
+        tabContentInfo.classList.add('active');
+
+
+        tabs.forEach(tab => {
+            tab.classList.remove('active');
+        })
+        tab.classList.add('active');
+    })
+})
+
